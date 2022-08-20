98b1c915bbf46786607ac4c0257f134ae22ccd59
Cristian Andres <barcandres91@gmail.com>
2022-08-20 09:52:02 -0500

    finalizacion de implementacion de spinner

--- Spinner/app/src/main/java/com/example/spinner/MainActivity.java
+++ Spinner/app/src/main/java/com/example/spinner/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +27,34 @@ public class MainActivity extends AppCompatActivity {
         spinner1=(Spinner) findViewById(R.id.spinner);
 
         String [] opciones ={"sumar","restar","multiplicar","dividir"};
+        ArrayAdapter <String> adaptadorSpinner=new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,opciones);
+        spinner1.setAdapter(adaptadorSpinner);//envia el spinner que se guardo en el arreglo al layaout
+    }
+    public void calcular(View view){
+        int num1 = Integer.parseInt(et1.getText().toString());
+        int num2 = Integer.parseInt(et2.getText().toString());
+        int operacion =0;
+        String resultado ="";
 
+        String seleccion = spinner1.getSelectedItem().toString();
+        switch (seleccion){
+            case "sumar":
+                //hace la operacion, la convierte en string y luego la envia al textview
+                tv.setText(resultado =String.valueOf(operacion = num1+num2));
+                break;
+            case "restar":
+                tv.setText(resultado =String.valueOf(operacion = num1-num2));
+                break;
+            case "multiplicar":
+                tv.setText(resultado =String.valueOf(operacion = num1*num2));
+                break;
+            case "dividir":
+                if (num2!=0) {
+                    tv.setText(resultado =String.valueOf(operacion = num1/num2));
+                }else{
+                    Toast.makeText(this, "no se puede dividir entre 0", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
